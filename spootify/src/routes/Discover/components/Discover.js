9187a0d845dad62f1704f7404f80075ddb5976ee
fiorellasaro <fiorellasaro27@gmail.com>
2021-08-24 19:52:12 -0500

    api spotify

--- spootify/src/routes/Discover/components/Discover.js
+++ spootify/src/routes/Discover/components/Discover.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component} from 'react';
+import axios from 'axios'
 import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
 import '../styles/_discover.scss';
+import api from '../../../config';
 
 export default class Discover extends Component {
   constructor() {
@@ -12,9 +14,72 @@ export default class Discover extends Component {
       categories: []
     };
   }
+  
+  componentDidMount() {
+    axios({
+      method: 'GET',
+      url: `${api.api.baseUrl}/new-releases`,
+      headers: {
+        'Authorization': `Bearer ${api.api.token}` 
+      }
+    })
+      .then((response) => {
+        this.setState({
+          newReleases: response.data.albums.items
+        });
+      })
+      .catch((error) => {
 
+        console.log(error);
+
+      });
+
+      axios({
+        method: 'GET',
+        url: `${api.api.baseUrl}/featured-playlists`,
+        headers: {
+          'Authorization': `Bearer ${api.api.token}` 
+        }
+      })
+        .then((response) => {
+          this.setState({
+            playlists: response.data.playlists.items
+          });
+          
+        })
+        .catch((error) => {
+  
+          console.log(error);
+  
+        });
+
+        axios({
+          method: 'GET',
+          url: `${api.api.baseUrl}/categories`,
+          headers: {
+            'Authorization': `Bearer ${api.api.token}` 
+          }
+        })
+          .then((response) => {
+          
+            this.setState({
+              categories: response.data.categories.items
+            });
+            
+          })
+          .catch((error) => {
+    
+            console.log(error);
+    
+          });
+   }
   render() {
+
+
     const { newReleases, playlists, categories } = this.state;
+
+
+
 
     return (
       <div className="discover">
